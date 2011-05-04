685d1c3c19995106a9d06c3684379130428c3f7e
Florian Kruse <florian.kruse@tu-dortmund.de>
2011-05-04 11:52:27 +0000

    added first version of Config base class

---
+++ Config/Config.h
@@ -0,0 +1,16 @@
+
+
+/** \brief DooFit Config base class.
+ *
+ * This is the base class for all DooFit config objects. Julian already wants to
+ * vomit after a few lines of code.
+ */
+class Config : public TObject {
+  
+ public:
+  
+ protected:
+  
+ private: 
+  
+};
