4253fd5e8f843f3b0db65c0660649fc10e9253f6
Florian Kruse <florian.kruse@tu-dortmund.de>
2011-05-24 15:21:01 +0000

    TestWiese:
       * enhanced program options example

--- TestWiese/ProgramOptions/program_options_test.cpp
+++ TestWiese/ProgramOptions/program_options_test.cpp
@@ -1,34 +1,61 @@
 #include <boost/program_options.hpp>
 #include <iostream>
 #include <iterator>
+#include <string>
+#include <vector>
+#include <algorithm>
 
 namespace po = boost::program_options;
 using namespace std;
 
+// A helper function to simplify the main part.
+template<class T>
+ostream& operator<<(ostream& os, const vector<T>& v)
+{
+  copy(v.begin(), v.end(), ostream_iterator<T>(cout, " ")); 
+  return os;
+}
+
+
 int main(int ac, char* av[]) {
   try {
     
+    int opt;
     po::options_description desc("Allowed options");
     desc.add_options()
     ("help", "produce help message")
-    ("compression", po::value<int>(), "set compression level")
-    ;
+    ("optimization", po::value<int>(&opt)->default_value(10), 
+     "optimization level")
+    ("include-path,I", po::value< vector<string> >(), 
+     "include path")
+    ("input-file", po::value< vector<string> >(), "input file");
     
     po::variables_map vm;        
-    po::store(po::parse_command_line(ac, av, desc), vm);
-    po::notify(vm);    
+    po::positional_options_description p;
+    p.add("input-file", -1);
+    
+    po::store(po::command_line_parser(ac, av).
+              options(desc).positional(p).run(), vm);
+    po::notify(vm);
     
     if (vm.count("help")) {
       cout << desc << "\n";
       return 1;
     }
     
-    if (vm.count("compression")) {
-      cout << "Compression level was set to " 
-      << vm["compression"].as<int>() << ".\n";
-    } else {
-      cout << "Compression level was not set.\n";
+    if (vm.count("include-path"))
+    {
+      cout << "Include paths are: " 
+      << vm["include-path"].as< vector<string> >() << "\n";
     }
+    
+    if (vm.count("input-file"))
+    {
+      cout << "Input files are: " 
+      << vm["input-file"].as< vector<string> >() << "\n";
+    }
+    
+    cout << "Optimization level is " << opt << "\n";
   }
   catch(exception& e) {
     cerr << "error: " << e.what() << "\n";
