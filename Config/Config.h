f85060baa1307d0cd808ccaf8cde7e5608eb3cf5
Julian Wishahi <julian.wishahi@tu-dortmund.de>
2011-05-27 13:54:56 +0000

    Began to implement config base class with program_options.

--- Config/Config.h
+++ Config/Config.h
@@ -2,12 +2,37 @@
  *
  * This is the base class for all DooFit config objects. 
  */
+
+// STL
+#include <string>
+#include <vector>
+
+// Boost
+#include <boost/program_options.hpp>
+
+// ROOT
+
+// RooFit
+
 class Config {
   
  public:
+  Config();
+  Config(int argc, char* argv);
+  Config(const vector<string>& option_vector);
+  
+  ~Config;
+  
+  const boost::program_options::description&    desc();
+  const boost::program_options::variables_map&  var_map();
+  const vector<string>& unrec_options(); 
   
  protected:
+  std::string name_;
+  
+  boost::program_options::description   desc_;
+  boost::program_options::variables_map var_map_;
+  vector<string> unrec_options_;
   
  private: 
-  
 };
