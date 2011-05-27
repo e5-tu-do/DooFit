6b584407d3954f9171163b95a6085605dea48d4b
Julian Wishahi <julian.wishahi@tu-dortmund.de>
2011-05-27 13:32:38 +0000

    Sucessfully tested the usage of passing further unrecognizes arguments.

--- TestWiese/ProgramOptions/program_options_test.cpp
+++ TestWiese/ProgramOptions/program_options_test.cpp
@@ -19,51 +19,68 @@ ostream& operator<<(ostream& os, const vector<T>& v)
 
 int main(int ac, char* av[]) {
   try {
-    
-    int opt;
-    po::options_description desc("Allowed options");
-    desc.add_options()
-    ("help", "produce help message")
-    ("optimization", po::value<int>(&opt)->default_value(10), 
-     "optimization level")
-    ("include-path,I", po::value< vector<string> >(), 
-     "include path")
-    ("input-file", po::value< vector<string> >(), "input file");
-    
-    po::variables_map vm;        
-    po::positional_options_description p;
-    p.add("input-file", -1);
-    
-    po::store(po::command_line_parser(ac, av).
-              options(desc).positional(p).run(), vm);
-    po::notify(vm);
-    
-    if (vm.count("help")) {
-      cout << desc << "\n";
-      return 1;
-    }
-    
-    if (vm.count("include-path"))
-    {
-      cout << "Include paths are: " 
-      << vm["include-path"].as< vector<string> >() << "\n";
-    }
-    
-    if (vm.count("input-file"))
-    {
-      cout << "Input files are: " 
-      << vm["input-file"].as< vector<string> >() << "\n";
-    }
-    
-    cout << "Optimization level is " << opt << "\n";
+
+      po::options_description desc("Allowed options");
+      desc.add_options()
+          ("help", "produce help message")
+          ("compression", po::value<int>(), "set compression level")
+      ;
+      
+      po::options_description desc2("Allowed options 2");
+      desc2.add_options()
+          ("pleh2", "produce pleh2 message")
+          ("compressione", po::value<int>(), "set compressione level")
+      ;
+          
+
+      po::variables_map vm;
+      po::parsed_options parsed = po::command_line_parser(ac, av).options(desc).allow_unregistered().run();
+      
+      po::store(parsed, vm);
+      po::notify(vm);    
+
+      vector<string> to_pass_further = po::collect_unrecognized(parsed.options, po::exclude_positional);
+      
+      for (int i=0; i<to_pass_further.size(); ++i){
+        cout << to_pass_further[i] << endl;
+      }
+      
+      po::variables_map vm2;
+      po::parsed_options parsed2 = po::command_line_parser(to_pass_further).options(desc2).allow_unregistered().run();
+      
+      po::store(parsed2, vm2);
+      po::notify(vm2);
+
+      if (vm.count("help")) {
+          cout << desc << "\n";
+      }
+
+      if (vm.count("compression")) {
+          cout << "Compression level was set to " 
+               << vm["compression"].as<int>() << ".\n";
+      } else {
+          cout << "Compression level was not set.\n";
+      }
+      
+      if (vm2.count("pleh2")) {
+          cout << desc2 << "\n";
+      }
+
+      if (vm2.count("compressione")) {
+          cout << "Compressione level was set to " 
+               << vm2["compressione"].as<int>() << ".\n";
+      } else {
+          cout << "Compressione level was not set.\n";
+      }
+      
   }
   catch(exception& e) {
-    cerr << "error: " << e.what() << "\n";
-    return 1;
+      cerr << "error: " << e.what() << "\n";
+      return 1;
   }
   catch(...) {
-    cerr << "Exception of unknown type!\n";
+      cerr << "Exception of unknown type!\n";
   }
-  
+
   return 0;
 }
