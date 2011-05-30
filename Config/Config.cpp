79ed22732ae48a2a417b47338569e2acd7c98022
Florian Kruse <florian.kruse@tu-dortmund.de>
2011-05-30 15:42:27 +0000

    Config:    * implemented Config::CombineOptions() and added a todo remark

--- Config/Config.cpp
+++ Config/Config.cpp
@@ -13,13 +13,30 @@ Config::Config() :
   descs_hidden_(),
   unrec_options_()
 {
-  
+  DefineOptions();
+  CombineOptions();
 }
 
 Config::Config(int argc, char* argv){
-  // 
+  DefineOptions();
+  CombineOptions();
+  
+  /// \todo{ add a clever way to parse the config file. How to specify what the
+  ///        config file is without messing with the program_options objects 
+  ///        itself too much or requiring anything... }
 }
 
 Config::Config(const vector<string>& option_vector){
+  DefineOptions();
+  CombineOptions();
+}
+
+Config::CombineOptions() {
+  for (vector<po::options_description>::const_iterator it = descs_.begin(); it < descs_.end(); ++it) {
+    desc_.add(*it);
+  }
   
+  for (vector<po::options_description>::const_iterator it = descs_hidden_.begin(); it < descs_hidden_.end(); ++it) {
+    desc_hidden_.add(*it);
+  }
 }
