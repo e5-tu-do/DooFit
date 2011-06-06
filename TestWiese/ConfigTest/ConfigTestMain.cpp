0f45179d90ccb130a7fbed8e770bedac8f04d01c
Florian Kruse <florian.kruse@tu-dortmund.de>
2011-06-06 16:16:51 +0000

    Config:     * expanded todo items with possible solution

--- TestWiese/ConfigTest/ConfigTestMain.cpp
+++ TestWiese/ConfigTest/ConfigTestMain.cpp
@@ -9,12 +9,17 @@ int main(int argc, char *argv[]) {
    *       Config::PrintHelp(). I'd like this to be rather automatic without the
    *       need to invoke something like Config::CheckforHelpFlagAndPrintHelp() 
    *       in every binary or main().
+   *       Solution: Capture manually.
    * \todo How can printing of all set options be handled without having to 
    *       invoke all Config::Print() functions by hand? Imagine having created 
    *       a bunch of derived Config objects and then you want to print/dump the
    *       complete set of options at different points in the code. It would be
    *       rather not nice to have to always use a block of Print() statements
    *       there.
+   *       Solution: use static vector to store all config objects (incl. 
+   *       checking for uniqueness by name) and have a Config::PrintAll() 
+   *       function that can loop over it. Make it private so that no derived 
+   *       class can tamper with it.
    */
   
   ConfigTest config;
