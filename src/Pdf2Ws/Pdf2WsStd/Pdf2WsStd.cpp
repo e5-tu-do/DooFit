c260f2f80628d578a5a5ef94bf41587667eaa7d1
Julian Wishahi <julian.wishahi@tu-dortmund.de>
2011-08-22 12:20:48 +0000

    Beginning to write Pdf2WsStd class.

---
+++ src/Pdf2Ws/Pdf2WsStd/Pdf2WsStd.cpp
@@ -0,0 +1,5 @@
+
+// from Project
+#include "Pdf2WsStd.h"
+#include "Pdf2WsStdConfig.h"
+
---
+++ src/Pdf2Ws/Pdf2WsStd/Pdf2WsStd.h
@@ -0,0 +1,14 @@
+/** \class Pdf2WsStd
+ *  \brief Standard Pdf2Ws. Adds Pdf object to a workspace.
+ *
+ *  This Pdf2Ws class is meant as standard implementation. The configuration in
+ *  the Pdf2WsStdConfig class is used to create the correct Pdfs on a workspace.
+ *  \see Pdf2WsStdConfig
+ */
+
+#ifndef PDF2WSSTD_h
+#define PDF2WSSTD_h
+
+
+
+#endif // PDF2WSSTD_h
