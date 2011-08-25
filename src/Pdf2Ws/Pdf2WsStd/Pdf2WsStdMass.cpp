6fa35b6fbe64ff1b9cd434b4f56d6968120ce8b1
Julian Wishahi <julian.wishahi@tu-dortmund.de>
2011-08-25 14:31:02 +0000

    Added doxy comment.

--- src/Pdf2Ws/Pdf2WsStd/Pdf2WsStdMass.cpp
+++ src/Pdf2Ws/Pdf2WsStd/Pdf2WsStdMass.cpp
@@ -1,3 +1,9 @@
+/** @namespace PdfWsStd::Mass
+ *
+ *  @brief PdfWsStd::Mass namespace for mass pdf adders.
+ *
+ */
+
 // STL
 #include <iostream>
 
@@ -24,6 +30,18 @@
 using namespace std;
 using namespace Pdf2WsStd;
 
+
+/**
+ *  @brief Function to add a single Gaussian mass pdf to the workspace.
+ *
+ *  This function adds a RooGaussian pdf with name @a pdf_name to the workspace.
+ *  @param ws The target workspace.
+ *  @param pdf_name The name of the pdf
+ *  @param pdf_desc The description of the pdf. Normally a latex like formula.
+ *  @param var_mass_name Name of the mass variable.
+ *  @param par_mean_name Name of the Gaussian mean parameter, typically the mass to be fit.
+ *  @param par_sigma_name Name of the standard deviation parameter of the Gaussian. 
+ */
 void Pdf2WsStd::Mass::Gaussian(RooWorkspace* ws, 
                                const TString& pdf_name, const TString& pdf_desc,
                                const TString& var_mass_name, 
