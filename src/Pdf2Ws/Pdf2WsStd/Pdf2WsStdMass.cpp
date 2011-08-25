d0374668960fc569e8064fdf11c4111b210ebfe1
Julian Wishahi <julian.wishahi@tu-dortmund.de>
2011-08-25 14:47:42 +0000

    Corrected doxygen docu.

--- src/Pdf2Ws/Pdf2WsStd/Pdf2WsStdMass.cpp
+++ src/Pdf2Ws/Pdf2WsStd/Pdf2WsStdMass.cpp
@@ -1,9 +1,3 @@
-/** @namespace PdfWsStd::Mass
- *
- *  @brief PdfWsStd::Mass namespace for mass pdf adders.
- *
- */
-
 // STL
 #include <iostream>
 
@@ -31,17 +25,7 @@ using namespace std;
 using namespace Pdf2WsStd;
 
 
-/**
- *  @brief Function to add a single Gaussian mass pdf to the workspace.
- *
- *  This function adds a RooGaussian pdf with name @a pdf_name to the workspace.
- *  @param ws The target workspace.
- *  @param pdf_name The name of the pdf
- *  @param pdf_desc The description of the pdf. Normally a latex like formula.
- *  @param var_mass_name Name of the mass variable.
- *  @param par_mean_name Name of the Gaussian mean parameter, typically the mass to be fit.
- *  @param par_sigma_name Name of the standard deviation parameter of the Gaussian. 
- */
+
 void Pdf2WsStd::Mass::Gaussian(RooWorkspace* ws, 
                                const TString& pdf_name, const TString& pdf_desc,
                                const TString& var_mass_name, 
@@ -60,5 +44,3 @@ void Pdf2WsStd::Mass::Gaussian(RooWorkspace* ws,
   
   ws->import(RooGaussian(pdf_name, pdf_desc, *var_mass, *par_mean, *par_sigma));
 }
-
-
--- src/Pdf2Ws/Pdf2WsStd/Pdf2WsStdMass.h
+++ src/Pdf2Ws/Pdf2WsStd/Pdf2WsStdMass.h
@@ -1,3 +1,8 @@
+/** @namespace PdfWsStd::Mass
+ *
+ *  @brief PdfWsStd::Mass namespace for mass pdf adders.
+ *
+ */
 #ifndef PDF2WSSTDMASS_h
 #define PDF2WSSTDMASS_h
 
@@ -10,10 +15,21 @@ class RooWorkspace;
 class TString;
 
 namespace Pdf2WsStd{ namespace Mass{
- void Gaussian(RooWorkspace* ws, 
-               const TString& pdf_name, const TString& pdf_desc,
-               const TString& var_mass, 
-               const TString& par_mean, const TString& par_sigma);
+  /**
+   *  @brief Function to add a single Gaussian mass pdf to the workspace.
+   *
+   *  This function adds a RooGaussian pdf with name @a pdf_name to the workspace.
+   *  @param ws The target workspace.
+   *  @param pdf_name The name of the pdf
+   *  @param pdf_desc The description of the pdf. Normally a latex like formula.
+   *  @param var_mass_name Name of the mass variable.
+   *  @param par_mean_name Name of the Gaussian mean parameter, typically the mass to be fit.
+   *  @param par_sigma_name Name of the standard deviation parameter of the Gaussian. 
+   */
+  void Gaussian(RooWorkspace* ws, 
+                const TString& pdf_name, const TString& pdf_desc,
+                const TString& var_mass, 
+                const TString& par_mean, const TString& par_sigma);
    
 }}
 
