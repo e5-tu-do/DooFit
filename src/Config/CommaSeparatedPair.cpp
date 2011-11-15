ebe1821c0793bcecda403c86ccbfc0d907b6d9c4
Florian Kruse <florian.kruse@tu-dortmund.de>
2011-11-15 21:10:30 +0000

    Config:    * added Config::CommaSeparatedPair

---
+++ src/Config/CommaSeparatedPair.cpp
@@ -0,0 +1,27 @@
+#include "Config/CommaSeparatedPair.h"
+
+// from STL
+
+// from Project
+#include "Utils/MsgStream.h"
+
+using namespace std;
+
+namespace Config {
+  void CommaSeparatedPair::Parse(string str) {
+    vector<string> elements = DecomposeString(str);
+    
+    // number of elements must be two
+    if (elements.size() != 2) {
+      serr << "ERROR in CommaSeparatedPair::Parse(std::string): String '" << str << "' does not contain exactly two comma-separated strings." << endmsg;
+      throw;
+    }
+    
+    first_ = elements[0];
+    second_ = elements[1];
+  }
+  
+  void CommaSeparatedPair::Print(std::ostream& os) const {
+    os << first_ << "->" << second_;
+  }
+}
---
+++ src/Config/CommaSeparatedPair.h
@@ -0,0 +1,98 @@
+#ifndef COMMASEPARATEDPAIR_h
+#define COMMASEPARATEDPAIR_h
+
+// from project
+#ifndef __CINT__
+#include "Config/AbsTypeCommaSeparated.h"
+#else
+// ROOT Cint hacks...
+#include "../Config/AbsTypeCommaSeparated.h"
+#endif /* __CINT __ */
+
+namespace Config {
+  /** @class CommaSeparatedPair
+   *  @brief A simple comma separated pair of two strings.
+   *
+   *  This class is merely a container for Config objects to store a pair of two
+   *  strings, separated by a comme. As it's inheriting from 
+   *  AbsTypeCommaSeparated, parsing from command line or config file
+   *  is possible. 
+   *
+   *  It contains variables first and second whose string representation is 
+   *  
+   *  @verbatim first,second @endverbatim
+   * 
+   *  @see AbsTypeCommaSeparated
+   *  @see ConfigTestAbstractType
+   */
+  
+  class CommaSeparatedPair : public AbsTypeCommaSeparated {
+  public:
+    /**
+     *  @brief Default constructor for CommaSeparatedPair
+     */
+    CommaSeparatedPair() {}
+    
+    /**
+     *  @brief Destructor for CommaSeparatedPair
+     */
+    ~CommaSeparatedPair() {}
+    
+    /**
+     *  @brief Parse a given string
+     *
+     *  The given string @a str is parsed. If parsing is not successful, an 
+     *  exception will be thrown. The string is supposed to be a comma-separated 
+     *  list formatted like this:
+     *
+     *  @verbatim first,second @endverbatim
+     *
+     *  @see CommaSeparatedPair::first_
+     *  @see CommaSeparatedPair::second_
+     *
+     *  @param str string to parse
+     */
+    void Parse(std::string str);
+    
+    /**
+     *  @brief Print this object to an std::ostream
+     *
+     *  This function is used to print the object. Calling this function directly
+     *  is not necessary as it can be streamed directly to any ostream via 
+     *  operator<<(std::ostream&, const AbsTypeCommaSeparated&).
+     *
+     *  @param os ostream to print to
+     */
+    virtual void Print(std::ostream& os) const;
+    
+    /**
+     *  @brief Getter for first string
+     */
+    std::string first() const {return first_;}
+    
+    /**
+     *  @brief Getter for second string
+     */
+    std::string second() const {return second_;}
+    
+  protected:
+    
+  private:
+    /**
+     *  @brief First string
+     */
+    std::string first_;
+    
+    /**
+     *  @brief Second string
+     */
+    std::string second_;
+    
+    /**
+     *  @brief ClassDef statement for CINT dictionary generation
+     */
+    ClassDef(CommaSeparatedPair,1);
+  };
+}
+
+#endif // COMMASEPARATEDPAIR_h
