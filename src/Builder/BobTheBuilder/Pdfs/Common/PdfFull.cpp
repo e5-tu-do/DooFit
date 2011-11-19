db41bd4e995ea26fcb99fa8a9d540e2be4a83127
Julian Wishahi <julian.wishahi@tu-dortmund.de>
2011-11-19 18:36:29 +0000

    Begun to implement PdfFull.

--- src/Builder/BobTheBuilder/Pdfs/Common/PdfFull.cpp
+++ src/Builder/BobTheBuilder/Pdfs/Common/PdfFull.cpp
@@ -1,1 +1,21 @@
 #include "Builder/BobTheBuilder/Pdfs/Common/PdfFull.h"
+
+// from boost
+
+using namespace Builder::BobTheBuilder;
+
+using namespace std;
+using namespace boost;
+using namespace boost::property_tree;
+
+PdfFull::PdfFull(){
+  
+}
+
+PdfFull::~PdfFull(){
+  
+}
+  
+void PdfFull::Initialize( const boost::property_tree::ptree::value_type& pt_head ){
+  
+}
--- src/Builder/BobTheBuilder/Pdfs/Common/PdfFull.h
+++ src/Builder/BobTheBuilder/Pdfs/Common/PdfFull.h
@@ -3,10 +3,15 @@
 
 
 // from STL
+#include <map>
 #include <string>
 
 // from boost
 #include <boost/property_tree/ptree.hpp>
+#include <boost/shared_ptr.hpp>
+
+// from project
+#include "Builder/BobTheBuilder/Pdfs/Common/SubPdfFull.h"
 
 namespace Builder{ namespace BobTheBuilder{
 
@@ -15,13 +20,16 @@ class PdfFull{
   PdfFull();
   ~PdfFull();
 
-  void Initialize( const boost::property_tree::ptree::value_type& pt_head );
+  void Initialize( const boost::property_tree::ptree::value_type& pt_head  );
 
  protected:
 
  private:
   std::string name_;
   std::string desc_; 
+  bool extended_;
+  
+  std::map< std::string, boost::shared_ptr< SubPdfFull > > map_suppdfs_;
   
 };
   
