7e5c51c9d99d29c31d948ba586b8f1e313acd07c
Florian Kruse <florian.kruse@tu-dortmund.de>
2012-01-28 20:57:18 +0000

    adding example project which uses DooFit as external input

---
+++ examples/DooFitExternalUsage/cmake/Modules/FindDooFit.cmake
@@ -0,0 +1,14 @@
+MESSAGE(STATUS "Looking for DooFit...")
+
+SET(DooFit_FOUND FALSE)
+
+IF(IS_DIRECTORY $ENV{DOOFITSYS})
+  SET(DooFit_BASE $ENV{DOOFITSYS})
+  SET(DooFit_FOUND TRUE)
+  SET(DooFit_INCLUDES "${DooFit_BASE}/include")
+  SET(DooFit_INCLUDE_DIR "${DooFit_BASE}/include")
+  SET(DooFit_LIBRARY_DIR "${DooFit_BASE}/lib")
+  SET(DooFit_LIBRARIES "-lToy -lPdf2WsStd -lConfig -lUtils")
+  #SET(LD_LIBRARY_PATH ${LD_LIBRARY_PATH} ${DooFit_LIBRARY_DIR})
+  MESSAGE(STATUS "Looking for DooFit... - found at ${DooFit_BASE}")
+ENDIF(IS_DIRECTORY $ENV{DOOFITSYS})
