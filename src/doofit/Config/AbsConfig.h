66697cf4f8ba55c70bad5685c1d35f559e6235cd
Florian Kruse <florian.kruse@tu-dortmund.de>
2012-02-02 15:14:30 +0000

    doxygen

--- src/doofit/Config/AbsConfig.h
+++ src/doofit/Config/AbsConfig.h
@@ -38,7 +38,7 @@
  * as set above. If not installing into a standard location like @c /usr/local
  * or similar, you also have to set @c $LD_LIBRARY_PATH (on Linux) or 
  * @c $DYLD_LIBRARY_PATH (on Mac OS) to include @c $DOOFITSYS/lib. Best practice
- * probably is to set these in your @code ~/.bashrc @endcode or globally.
+ * probably is to set these in your <tt>~/.bashrc</tt> or globally.
  *
  * @code
  * export DOOFITSYS=/path/to/install/to
