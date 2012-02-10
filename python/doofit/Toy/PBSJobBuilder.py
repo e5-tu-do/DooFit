31e88c35d336069a50bac14baabc642558889fca
Florian Kruse <florian.kruse@tu-dortmund.de>
2012-02-10 14:37:14 +0000

    doxygenm

--- python/doofit/Toy/PBSJobBuilder.py
+++ python/doofit/Toy/PBSJobBuilder.py
@@ -19,49 +19,49 @@ import os, sys
 #  Syntax/Example for keywords in the proto job file:
 #  
 #  @code
-#  #!/bin/sh
-#  
-#  #PBS -N %(job_name)s
-#  #PBS -o %(out_file)s
-#  #PBS -e %(err_file)s
-#  #PBS -m n
-#  #PBS -M phidomuell@googlemail.com
-#  #PBS -j oe
-#  #PBS -l walltime=%(walltime)s
-#  #PBS -l nodes=1:ppn=%(num_cpu)s
-#  #PBS -l mem=5000mb
-#  export SCRATCH=/local/$USER/$PBS_JOBID
-#  
-#  echo "Auto-generated PBS script"
-#  echo "Sin2Beta toy studies starting at `date`"
-#  . SetupProject.sh DaVinci v29r3p1 
-#  echo $0
-#  echo $PBS_JOBID > %(log_file)s
-#  echo ${PBS_JOBID} >> %(log_file)s
-#  cd %(cwd)s
-#  
-#  for i in `seq %(seeds)s`;
-#  do
-#    time echo toy --seed=$i --output=result_%(job_number)s.root --num_cpu=%(num_cpu)s >> %(log_file)s
-#  done
-#  
-#  echo finished job number %(job_number)s
-#  @endcode
-#  
-#  The following keywords are supported:
-#  
-#  @li @c job_name: the individual job's name
-#  @li @c out_file: the stdout log file for the individual job
-#  @li @c err_file: the stderr log file for the individual job
-#  @li @c log_file: the log file for the individual job
-#  @li @c walltime: the walltime for the job
-#  @li @c num_cpu:  the number of CPUs to use
-#  @li @c seeds:    the seeds for the iterations in this individual job to use (will be generated as <tt>minseed maxseed</tt> compatible to the above @c for statement)
-#  @li @c job_number: the number of the individual job
+##!/bin/sh
 #
-#  @section pbsjobbuilder_calling Calling PBSJobBuilder.py
+##PBS -N %(job_name)s
+##PBS -o %(out_file)s
+##PBS -e %(err_file)s
+##PBS -m n
+##PBS -M phidomuell@googlemail.com
+##PBS -j oe
+##PBS -l walltime=%(walltime)s
+##PBS -l nodes=1:ppn=%(num_cpu)s
+##PBS -l mem=5000mb
+#export SCRATCH=/local/$USER/$PBS_JOBID
 #
-#  Call PBSJobBuilder.py like this:
+#echo "Auto-generated PBS script"
+#echo "Sin2Beta toy studies starting at `date`"
+#. SetupProject.sh DaVinci v29r3p1 
+#echo $0
+#echo $PBS_JOBID > %(log_file)s
+#echo ${PBS_JOBID} >> %(log_file)s
+#cd %(cwd)s
+#
+#for i in `seq %(seeds)s`;
+#do
+#  time echo toy --seed=$i --output=result_%(job_number)s.root --num_cpu=%(num_cpu)s >> %(log_file)s
+#done
+#
+#echo finished job number %(job_number)s
+#@endcode
+#
+#The following keywords are supported:
+#
+#@li @c job_name: the individual job's name
+#@li @c out_file: the stdout log file for the individual job
+#@li @c err_file: the stderr log file for the individual job
+#@li @c log_file: the log file for the individual job
+#@li @c walltime: the walltime for the job
+#@li @c num_cpu:  the number of CPUs to use
+#@li @c seeds:    the seeds for the iterations in this individual job to use (will be generated as <tt>minseed maxseed</tt> compatible to the above @c for statement)
+#@li @c job_number: the number of the individual job
+#
+#@section pbsjobbuilder_calling Calling PBSJobBuilder.py
+#
+#Call PBSJobBuilder.py like this:
 #
 #  @code 
 #  PBSJobBuilder.py proto_script job_base_name jobs_dir num_pbs_jobs num_iterations_per_job walltime num_cpu
