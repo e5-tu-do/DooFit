2ad6a172662c3153095d4c839646c65bbd2b84f3
Florian Kruse <florian.kruse@tu-dortmund.de>
2012-02-14 14:20:36 +0000

    doxygen

--- src/doofit/Toy/ToyStudyStd/ToyStudyStd.h
+++ src/doofit/Toy/ToyStudyStd/ToyStudyStd.h
@@ -68,6 +68,7 @@ namespace Toy {
      *  again. If the file will not be unlocked after some time, the tree will 
      *  be saved to an alternative file.
      *
+     *  @see ToyStudyStdConfig
      *  @see ToyStudyStdConfig::set_store_result_filename_treename()
      *
      *  @warning See warnings for doofit::Toy::ToyStudyStdConfig::set_store_result_filename_treename(const Config::CommaSeparatedPair&)
