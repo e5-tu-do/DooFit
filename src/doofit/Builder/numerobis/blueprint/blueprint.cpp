e03df17a355b1851f9ec7139aa472068dd04d9e9
Julian Wishahi <julian.wishahi@tu-dortmund.de>
2012-02-14 08:32:15 +0000

    Blueprint stub created.

---
+++ src/doofit/Builder/numerobis/blueprint/blueprint.cpp
@@ -0,0 +1,19 @@
+#include "doofit/Builder/numerobis/blueprint/blueprint.h"
+
+namespace doofit {
+namespace builder {
+namespace numerobis {
+namespace blueprint {
+
+Blueprint::Blueprint() {
+  
+}
+
+Blueprint::~Blueprint() {
+  
+}
+
+} // namespace blueprint 
+} // namespace numerobis 
+} // namespace builder 
+} // namespace doofit 
---
+++ src/doofit/Builder/numerobis/blueprint/blueprint.h
@@ -0,0 +1,24 @@
+#ifndef DOOFIT_BUILDER_NUMEROBIS_BLUEPRINT_BLUEPRINT_H
+#define DOOFIT_BUILDER_NUMEROBIS_BLUEPRINT_BLUEPRINT_H
+
+namespace doofit {
+namespace builder {
+namespace numerobis {
+namespace blueprint {
+
+class Blueprint {
+ public:
+  Blueprint();
+  virtual ~Blueprint();
+
+ private:
+  /* data */
+};
+  
+} // namespace blueprint 
+} // namespace numerobis 
+} // namespace builder 
+} // namespace doofit 
+
+
+#endif // DOOFIT_BUILDER_NUMEROBIS_BLUEPRINT_BLUEPRINT_H
