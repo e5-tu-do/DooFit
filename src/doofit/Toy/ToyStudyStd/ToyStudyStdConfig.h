ae19950f00064cd9871f31a44aff8f5ebb489079
Florian Kruse <florian.kruse@tu-dortmund.de>
2012-02-14 16:10:43 +0000

    doxygen

--- src/doofit/Toy/ToyStudyStd/ToyStudyStdConfig.h
+++ src/doofit/Toy/ToyStudyStd/ToyStudyStdConfig.h
@@ -187,7 +187,7 @@ namespace Toy {
     /**
      *  @brief File name and tree name to store fit result to
      *
-     *  @see const Config::CommaSeparatedPair& ToyStudyStdConfig::store_result_filename_treename
+     *  @see ToyStudyStdConfig::store_result_filename_treename
      */
     Config::CommaSeparatedPair store_result_filename_treename_;
     /**
