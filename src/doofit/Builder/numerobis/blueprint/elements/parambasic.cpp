364318afc1fdbbd13fedb8f4bd16575541bb08f9
Julian Wishahi <julian.wishahi@tu-dortmund.de>
2012-03-09 19:59:10 +0000

    Added parambasic.

---
+++ src/doofit/Builder/numerobis/blueprint/elements/parambasic.cpp
@@ -0,0 +1,66 @@
+#include "doofit/Builder/numerobis/blueprint/elements/parambasic.h"
+
+// from RooFit
+#include "RooRealVar.h"
+
+namespace doofit {
+namespace builder {
+namespace numerobis {
+namespace blueprint {
+namespace elements {
+
+ParamBasic::ParamBasic(){
+
+}
+
+ParamBasic::ParamBasic(const std::string& id_rel, const std::string& id_abs, 
+                       const std::string& desc, double val_init, double val_min, 
+                       double val_max, const std::string& unit) 
+    : val_init_(val_init)
+    , val_min_(val_min)
+    , val_max_(val_max)
+    , unit_(unit)
+{
+  set_id_rel(id_rel);
+  set_id_abs(id_abs);
+  set_desc(desc);
+  
+  set_initialized(true);
+  set_ready(true);
+}
+
+ParamBasic::ParamBasic(const std::string& id_rel, const std::string& id_abs, 
+                       const std::string& desc, double val_init, 
+                       const std::string& unit) 
+    : val_init_(val_init)
+    , val_min_(val_init_)
+    , val_max_(val_init_)
+    , unit_(unit)
+{
+  set_id_rel(id_rel);
+  set_id_abs(id_abs);
+  set_desc(desc);
+  
+  set_initialized(true);
+  set_ready(true);
+}
+
+ParamBasic::~ParamBasic() {
+
+}
+
+RooAbsReal* ParamBasic::CreateTempRooObj() {
+  RooRealVar* temp_realvar = NULL;
+  if (val_max_ == val_init_ && val_min_ == val_init_) {
+    temp_realvar = new RooRealVar(id_abs().c_str(), desc().c_str(), val_init(), unit().c_str());
+  } else {
+    temp_realvar = new RooRealVar(id_abs().c_str(), desc().c_str(), val_init(), val_min(), val_max(), unit().c_str());
+  }
+  return temp_realvar;
+}
+
+} // namespace elements 
+} // namespace blueprint 
+} // namespace numerobis 
+} // namespace builder 
+} // namespace doofit
---
+++ src/doofit/Builder/numerobis/blueprint/elements/parambasic.h
@@ -0,0 +1,70 @@
+#ifndef DOOFIT_BUILDER_NUMEROBIS_BLUEPRINT_ElEMENTS_PARAMBASIC_H
+#define DOOFIT_BUILDER_NUMEROBIS_BLUEPRINT_ElEMENTS_PARAMBASIC_H
+
+// from STL
+#include <string>
+
+// from project
+#include "doofit/Builder/numerobis/blueprint/elements/paramelement.h"
+#include "doofit/Builder/numerobis/blueprint/elements/realvalelement.h"
+
+
+// forward declarations - RooFit
+class RooRealVar;
+
+namespace doofit {
+namespace builder {
+namespace numerobis {
+namespace blueprint {
+namespace elements {
+  
+class ParamBasic : public ParamElement {
+ public:
+  ParamBasic();
+  ParamBasic(const std::string& id_rel, const std::string& id_abs, const std::string& desc, double val_init, double val_min, double val_max, const std::string& unit="");  
+  ParamBasic(const std::string& id_rel, const std::string& id_abs, const std::string& desc, double val_init, const std::string& unit="");  
+
+  virtual ~ParamBasic();
+
+ protected:
+  RooAbsReal* CreateTempRooObj();
+
+  /** @brief Getter for val_init_ */
+  double val_init() const { return val_init_; }
+  /** @brief Setter for val_init_ */
+  void set_val_init(double val_init) { val_init_ = val_init; }
+
+  /** @brief Getter for val_min_ */
+  double val_min() const { return val_min_; }
+  /** @brief Setter for val_min_ */
+  void set_val_min(double val_min) { val_min_ = val_min; } 
+
+  /** @brief Getter for val_max_ */
+  double val_max() const { return val_max_; }
+  /** @brief Setter for val_max_ */
+  void set_val_max(double val_max) { val_max_ = val_max; } 
+
+  /** @brief Getter for unit_ */
+  std::string unit() const { return unit_; }
+  /** @brief Setter for unit_ */
+  void set_unit(const std::string& unit) { unit_ = unit; }
+
+
+
+
+ private:
+  double val_init_;
+  double val_min_;
+  double val_max_;
+  std::string unit_;
+
+  RooRealVar* roo_obj_;
+};
+  
+} // namespace elements
+} // namespace blueprint
+} // namespace numerobis
+} // namespace builder
+} // namespace doofit
+
+#endif // DOOFIT_BUILDER_NUMEROBIS_BLUEPRINT_ElEMENTS_PARAMBASIC_H
