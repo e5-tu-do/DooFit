fc1600fa4f9015499c8c061e39deeb655ea19b73
Julian Wishahi <julian.wishahi@tu-dortmund.de>
2012-03-26 20:01:14 +0000

    Blubbsel.

--- src/doofit/Builder/numerobis/blueprint/elements/registrar.cpp
+++ src/doofit/Builder/numerobis/blueprint/elements/registrar.cpp
@@ -55,50 +55,52 @@ bool Registrar::CheckReady(const std::string& element_name) {
   boost::ptr_map<std::string, Element>::iterator it_element = elements_.find(element_name);
   if (it_element == elements_.end()) return false;
   
-  Element* element = *it_element->second();
+  Element* element = it_element->second;
   
   namespace du = doofit::utils;
   //du::sdebug << "Checking ready for: " << element.id_abs() << du::endmsg;
   
   // Check if element is ready, if not, check if dependants are ready.
-  if (!element.ready()) {
-    const std::vector<std::string>& dep = element.dependants();
+  if (!element->ready()) {
+    const std::vector<std::string>& dep = element->dependants();
     bool allready = true;
     
-    std::map<std::string, Element*>::iterator it_el;
+    boost::ptr_map<std::string, Element>::iterator it_el;
     BOOST_FOREACH (std::string it_dep, dep) {
       it_el = elements_.find(it_dep);
       if (it_el == elements_.end()) {
         allready = false;
       } else {
-        allready &= CheckReady(*it_el->second);
+        allready &= CheckReady(it_el->second->id_abs());
       }  
     } 
-    if (allready) element.set_ready(true);
+    if (allready) element->set_ready(true);
   }
   
-  return element.ready();
+  return element->ready();
 }
 
-RooAbsArg* Registrar::Register(RooWorkspace* ws, Element &element) {
-  if (CheckReady(element)) {
-    const std::vector<std::string>& dep_ids = element.dependants();
+RooAbsArg* Registrar::Register(RooWorkspace* ws, const std::string& element_name) {
+  if (CheckReady(element_name)) {
+    Element* element = elements_.find(element_name)->second;
     
-    // Iterate ove dependants and check if dependants exist in elements_. 
+    const std::vector<std::string>& dep_ids = element->dependants();
+    
+    // Iterate over dependants and check if dependants exist in elements_. 
     // If not, throw exception.
     // If yes, register them and invoke AddToWorkspace
     std::vector<RooAbsArg*> dep_elements;
-    std::map<std::string, Element*>::iterator it_el;
+    boost::ptr_map<std::string, Element>::iterator it_el;
     BOOST_FOREACH(std::string it_dep, dep_ids) {
       it_el = elements_.find(it_dep);
       if (it_el == elements_.end()) {
         throw UnexpectedException();
       } else {
-        Element& dep_element = *it_el->second;
-        dep_elements.push_back(Register(ws, dep_element));
+        Element* dep_element = it_el->second;
+        dep_elements.push_back(Register(ws, dep_element->id_abs()));
       }
     }
-    return element.AddToWorkspace(ws, dep_elements);
+    return element->AddToWorkspace(ws, dep_elements);
   }
   return NULL;
 }
