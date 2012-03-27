b38669a6da5ae05eacbce24a99568d90082a1b64
Julian Wishahi <julian.wishahi@tu-dortmund.de>
2012-03-27 13:02:05 +0000

    Added constructors.

--- src/doofit/Builder/numerobis/blueprint/pdfs/pdf.cpp
+++ src/doofit/Builder/numerobis/blueprint/pdfs/pdf.cpp
@@ -1,1 +1,30 @@
 #include "pdf.h"
+
+namespace doofit {
+namespace builder {
+namespace numerobis {
+namespace blueprint {
+namespace pdfs {
+
+Pdf::Pdf() 
+    : id_rel_("NoRelID")
+    , id_abs_("NoAbslID")
+    , initialized_(false)
+    , ready_(false)
+    , onworkspace_(false)
+{ }
+
+Pdf::Pdf(const std::string& id_rel, const std::string& id_abs)
+    : id_rel_(id_rel)
+    , id_abs_(id_abs)
+    , initialized_(false)
+    , ready_(false)
+    , onworkspace_(false)
+{ }
+
+
+} // namespace pdfs 
+} // namespace blueprint 
+} // namespace numerobis 
+} // namespace builder 
+} // namespace doofit 
