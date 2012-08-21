b400644d8c9e2b384980c1fb85ebdc089f1e72d3
Florian Kruse <florian.kruse@tu-dortmund.de>
2012-08-21 08:10:10 +0000

    trying to fix some doxygen

--- python/doofit/Toy/PBSJobBuilder.py
+++ python/doofit/Toy/PBSJobBuilder.py
@@ -22,31 +22,31 @@ from optparse import OptionParser
 #  @code
 # #!/bin/sh
 #
-##PBS -N %(job_name)s
-##PBS -o %(out_file)s
-##PBS -e %(err_file)s
-##PBS -m n
-##PBS -M phidomuell@googlemail.com
-##PBS -j oe
-##PBS -l walltime=%(walltime)s
-##PBS -l nodes=1:ppn=%(num_cpu)s
-##PBS -l mem=5000mb
-#export SCRATCH=/local/$USER/$PBS_JOBID
+# #PBS -N %(job_name)s
+# #PBS -o %(out_file)s
+# #PBS -e %(err_file)s
+# #PBS -m n
+# #PBS -M phidomuell@googlemail.com
+# #PBS -j oe
+# #PBS -l walltime=%(walltime)s
+# #PBS -l nodes=1:ppn=%(num_cpu)s
+# #PBS -l mem=5000mb
+# export SCRATCH=/local/$USER/$PBS_JOBID
 #
-#echo "Auto-generated PBS script"
-#echo "Sin2Beta toy studies starting at `date`"
-#. SetupProject.sh DaVinci v29r3p1 
-#echo $0
-#echo $PBS_JOBID > %(log_file)s
-#echo ${PBS_JOBID} >> %(log_file)s
-#cd %(cwd)s
+# echo "Auto-generated PBS script"
+# echo "Sin2Beta toy studies starting at `date`"
+# . SetupProject.sh DaVinci v29r3p1 
+# echo $0
+# echo $PBS_JOBID > %(log_file)s
+# echo ${PBS_JOBID} >> %(log_file)s
+# cd %(cwd)s
 #
-#for i in `seq %(seeds)s`;
-#do
-#  time echo toy --scanstart=%(scan1_start)s --scanend=%(scan1_end)s --scan2start=%(scan2_start)s --scan2end=%(scan2_end)s --seed=$i --output=%(jobs_dir)s/result_%(job_number)s.root --num_cpu=%(num_cpu)s >> %(log_file)s
-#done
+# for i in `seq %(seeds)s`;
+# do
+#   time echo toy --scanstart=%(scan1_start)s --scanend=%(scan1_end)s --scan2start=%(scan2_start)s --scan2end=%(scan2_end)s --seed=$i --output=%(jobs_dir)s/result_%(job_number)s.root --num_cpu=%(num_cpu)s >> %(log_file)s
+# done
 #
-#echo finished job number %(job_number)s
+# echo finished job number %(job_number)s
 #@endcode
 #
 #The following keywords are supported:
