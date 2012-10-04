040c9ce41094e1ae7f7ad448c673dd4e425a51c5
Florian Kruse <florian.kruse@tu-dortmund.de>
2012-10-04 10:55:23 +0000

    first version of install scripts

---
+++ install-all.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+source /lhcbsoft/tu-dortmund/LHCbSoftwareSetup.sh x86_64-slc5-gcc43-opt > /dev/null 2>&1
+
+for ver in `SetupProject.sh DaVinci --list-versions | cut -d " " -f 1`
+do
+  ./install.sh $ver $1
+done
---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+uuid=`uuidgen -r`
+
+function catch_errors {
+  echo "Could not complete build for DaVinci $1"
+  rm -rf /tmp/$uuid
+  exit 1
+}
+
+trap catch_errors ERR
+
+echo "Installing software for DaVinci $1"
+source /lhcbsoft/tu-dortmund/LHCbSoftwareSetup.sh x86_64-slc5-gcc43-opt > /dev/null 2>&1
+source SetupProject.sh DaVinci $1 > /dev/null 2>&1
+mkdir /tmp/$uuid
+cd /tmp/$uuid
+echo "Starting build"
+echo
+
+cmake -DCMAKE_INSTALL_PREFIX=/doosoft/DooFit/$ROOTVERS $2
+make VERBOSE="" -j 4
+sudo make install
+cd /
+rm -rf /tmp/$uuid
+
+echo
+echo "Build successful for DaVinci $1"
+echo
+
