3767ec9818cdbd68ba607f88324eb771a85aacde
Florian Kruse <florian.kruse@tu-dortmund.de>
2012-10-08 15:00:15 +0000

    renamed install scripts

--- install-all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source /lhcbsoft/tu-dortmund/LHCbSoftwareSetup.sh x86_64-slc5-gcc43-opt > /dev/null 2>&1
-
-for ver in `SetupProject.sh DaVinci --list-versions | cut -d " " -f 1`
-do
-  bash ./install.sh $ver $1
-done
