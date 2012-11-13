fd90a69e860f3b87ee524da16e7bacf975ecca07
Julian Wishahi <julian.wishahi@tu-dortmund.de>
2012-11-13 16:58:27 +0000

    Additional changes with pragma stuff in LinkDef.

--- src/doofit/extraroopdfs/RooBMixAsymDecay_LinkDef.h
+++ src/doofit/extraroopdfs/RooBMixAsymDecay_LinkDef.h
@@ -3,9 +3,9 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
+#pragma link C++ namespace doofit::extraroopdfs;
 #pragma link C++ nestedclass;
 #pragma link C++ nestedtypedef;
 
-#pragma link C++ namespace doofit::extraroopdfs;
 #pragma link C++ class doofit::extraroopdfs::RooBMixAsymDecay;
 #endif
