6ce1928936abd6dbba2491da7692ef9069a3ef29
Florian Kruse <florian.kruse@tu-dortmund.de>
2013-09-24 10:40:39 +0200

    some fixes to build DooFit with extra PDFs

--- cmake/Modules/FindROOT.cmake
+++ cmake/Modules/FindROOT.cmake
@@ -121,7 +121,7 @@ IF (ROOT_FOUND)
 #  REMOVE_FROM_LIST(root_flags "${root_libs_all}" "${root_library}")
 
   SET(ROOT_LIBRARIES ${root_flags})
-  SET(ROOFIT_LIBRARIES "-lRooFit -lRooFitCore -lRooStats -lHtml -lMinuit -lThread -lFoam -lMathMore")
+  SET(ROOFIT_LIBRARIES "-lRooFit -lRooFitCore -lRooStats -lHtml -lMinuit -lThread -lFoam -lMatrix -lMathMore")
 
   # Make variables changeble to the advanced user
   MARK_AS_ADVANCED( ROOT_LIBRARY_DIR ROOT_INCLUDE_DIR ROOT_DEFINITIONS)
