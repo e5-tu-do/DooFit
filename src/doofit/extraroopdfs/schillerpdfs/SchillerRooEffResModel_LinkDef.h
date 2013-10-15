9976a78f66a37aa59230feccce6580beaffe68fe
Florian Kruse <florian.kruse@tu-dortmund.de>
2013-10-15 16:45:01 +0200

    compiling, yay with Schiller files

---
+++ src/doofit/extraroopdfs/schillerpdfs/SchillerRooEffResModel_LinkDef.h
@@ -0,0 +1,11 @@
+#ifdef __CINT__
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+// #pragma link C++ namespace doofit;
+// #pragma link C++ namespace doofit::extraroopdfs;
+#pragma link C++ nestedclass;
+#pragma link C++ nestedtypedef;
+#pragma link C++ class doofit::extraroopdfs::schillerpdfs::RooEffResModel;
+#endif
