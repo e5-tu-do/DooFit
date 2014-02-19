7273a5180a05a693e1a621c2c0614745ccf27588
Florian Kruse <florian.kruse@tu-dortmund.de>
2014-02-19 15:34:25 +0100

    EasyFit: fixing times to be correct now

--- src/doofit/fitter/easyfit/EasyFit.cpp
+++ src/doofit/fitter/easyfit/EasyFit.cpp
@@ -185,8 +185,8 @@ void EasyFit::ExecuteFit() {
     std::clock_t c_end = std::clock();
     auto t_end = std::chrono::high_resolution_clock::now();
     
-    time_cpu_  = std::chrono::duration_cast<std::chrono::milliseconds>(t_end - t_start).count();
-    time_real_ = 1000.0 * (c_end-c_start) / CLOCKS_PER_SEC;
+    time_real_ = std::chrono::duration_cast<std::chrono::milliseconds>(t_end - t_start).count();
+    time_cpu_  = 1000.0 * (c_end-c_start) / CLOCKS_PER_SEC;
     
     fitted_ = true;
   }
