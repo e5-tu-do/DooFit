8d6802c6427a3187277e62b2d4c418eab1c26bfa
Florian Kruse <florian.kruse@tu-dortmund.de>
2014-04-29 10:14:03 +0200

    JobMonitor: update

--- scripts/jobmonitor.sh
+++ scripts/jobmonitor.sh
@@ -2,7 +2,9 @@
 
 num_running=$(qstat | grep fkruse | egrep ":[0-9]{2} R" | wc -l)
 num_jobs=$(qstat | grep fkruse | wc -l)
+num_running_total=$(qstat -q | tail -n 1 | awk '{ print $1; }')
+num_queued_total=$(qstat -q | tail -n 1 | awk '{ print $2; }')
 
-echo "Jobs: ${num_running}/${num_jobs}"
+echo "Own jobs (R/T): ${num_running}/${num_jobs} [all jobs (R/Q): ${num_running_total}/${num_queued_total}]"
 qstat | grep fkruse
 
