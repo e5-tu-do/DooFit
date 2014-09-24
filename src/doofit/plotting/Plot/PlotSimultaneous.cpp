58dda99b784f644dae56c1245b2fbe9a75cff689
Florian Kruse <florian.kruse@tu-dortmund.de>
2014-09-24 17:43:04 +0200

    PlotSimultaneous: Experimental Plotting of summed categories (all Long, Down, etc.)

--- src/doofit/plotting/Plot/PlotSimultaneous.cpp
+++ src/doofit/plotting/Plot/PlotSimultaneous.cpp
@@ -51,6 +51,8 @@ void PlotSimultaneous::PlotHandler(ScaleType sc_y, std::string suffix) const {
   //TList* data_split = data.split(sim_cat);
   std::string plot_name;
   
+  std::map<std::string, RooCategory*> categories;
+
   RooCatType* sim_cat_type = NULL;
   TIterator* sim_cat_type_iter = sim_cat.typeIterator();
   int num_slices = 0;
@@ -74,6 +76,9 @@ void PlotSimultaneous::PlotHandler(ScaleType sc_y, std::string suffix) const {
           if (cat != NULL) {
             if (cut_string.length() > 0) cut_string = cut_string + "&&";
             cut_string = cut_string + cat->GetName() + "==" + std::to_string(cat->getIndex());
+            if (categories.count(cat->GetName()) == 0) {
+              categories.insert(std::pair<std::string,RooCategory*>(cat->GetName(),cat));
+            }
           } else {
             serr << "Error in PlotSimultaneous::PlotHandler(...): Cannot handle category component " << arg->GetName() << endmsg;
           }
@@ -90,6 +95,8 @@ void PlotSimultaneous::PlotHandler(ScaleType sc_y, std::string suffix) const {
       RooAbsData* sub_data2 = data_nonconst_fucking_roofit.reduce(Cut(cut_string.c_str()));
       RooAbsData& sub_data = *sub_data2;
       
+
+
       if (&sub_data == NULL) {
         serr << "PlotSimultaneous::PlotHandler(...): sub dataset for category " << sim_cat_type->GetName() << " empty. Will not plot. " << endmsg;
       } else {
@@ -210,6 +217,148 @@ void PlotSimultaneous::PlotHandler(ScaleType sc_y, std::string suffix) const {
     }
   }
   
+  //if (false) {
+  for (auto pair_cat : categories) {
+    RooCategory* category = pair_cat.second;
+
+    TIterator* cat_type_iter = category->typeIterator();
+    RooCatType* cat_type = NULL;
+    while((cat_type=dynamic_cast<RooCatType*>(cat_type_iter->Next()))) {
+      category->setIndex(cat_type->getVal());
+      std::string cut_string = std::string(category->GetName()) + "==" + std::to_string(category->getIndex());
+      sdebug << "Plot for " << category->getLabel() << " with cut " << cut_string << endmsg;
+
+      RooAbsData* sub_data2 = data_nonconst_fucking_roofit.reduce(Cut(cut_string.c_str()));
+      RooAbsData& sub_data = *sub_data2;
+
+      // sub_data.Print();
+
+      if (sub_data2 == nullptr) {
+        serr << "PlotSimultaneous::PlotHandler(...): sub dataset for category " << cat_type->GetName() << " empty. Will not plot. " << endmsg;
+      } else {
+        double min,max;
+        RooRealVar var(dynamic_cast<const RooRealVar&>(dimension_));
+        sub_data.getRange(var, min, max);
+
+        plot_name = plot_name_ + "_" + cat_type->GetName();
+        Plot plot(config_plot_, dimension_, sub_data, pdf, components_regexps_, plot_name);
+        plot.plot_args_ = this->plot_args_;
+        plot.plot_range_ = this->plot_range_;
+                
+        // go through supplied cmd args and if necessary adapt ProjWData argument
+        bool project_arg_found         = false;
+        RooArgSet* set_project         = NULL;
+        RooAbsData* data_reduced       = NULL;
+        const RooAbsData* data_project = NULL;
+        bool binned_projection         = false;
+        const RooAbsData* data_all     = NULL;
+        for (std::vector<RooCmdArg>::iterator it = plot.plot_args_.begin(); it != plot.plot_args_.end(); ++it) {
+          if (std::string(it->GetName()) == "ProjData") {
+            data_all = dynamic_cast<const RooAbsData*>(it->getObject(1));
+            data_all = &sub_data;
+
+            sinfo << "Found ProjWData() argument. Will join with " << sim_cat.GetName() << " on same dataset." << endmsg;
+            project_arg_found = true;
+            binned_projection = it->getInt(0);
+            
+            // copy argset for projection and add simultaneous category variables
+            set_project = new RooArgSet(*dynamic_cast<const RooArgSet*>(it->getObject(0)));
+            
+            const RooSuperCategory* super_sim_cat = dynamic_cast<const RooSuperCategory*>(&sim_cat);
+            if (super_sim_cat) {
+              // The simultaneous category is a super category and will not be in the
+              // supplied dataset. Add input categories instead.
+              set_project->add(super_sim_cat->inputCatList());
+            } else {
+              set_project->add(sim_cat);
+            }
+            
+            // check for binned projection and if so generate binned dataset here to
+            // accelerate projection
+            if (binned_projection) {
+              sinfo << " Binned projection is requested. Will generate a binned dataset to accelerate projection." << endmsg;
+              
+              RooAbsData* data_proj = const_cast<RooAbsData*>(data_all);
+              data_reduced = data_proj->reduce(*set_project);
+              std::string name_data_hist = std::string(data_proj->GetName()) + "_hist";
+              data_project = new RooDataHist(name_data_hist.c_str(), "binned projection dataset", *data_reduced->get(), *data_reduced);
+              
+              sinfo << " Created binned dataset with " << data_project->numEntries() << " bins." << endmsg;
+              
+              unsigned int num_nonempty_bins = 0;
+              for (int i=0; i<data_project->numEntries(); ++i) {
+                data_project->get(i);
+                if (data_project->weight() != 0.0) {
+                  ++num_nonempty_bins;
+                }
+              }
+              double frac_nonempty = static_cast<double>(num_nonempty_bins)/static_cast<double>(data_project->numEntries());
+              if (frac_nonempty < 0.05) {
+                swarn << " The binned dataset contains only " << num_nonempty_bins << " non-empty bins, that is only " << frac_nonempty*100.0 << "% of all bins. You might want to optimize the binning of your projection variables." << endmsg;
+              }
+            } else {
+              RooAbsData* data_proj = const_cast<RooAbsData*>(data_all);
+              data_project = data_reduced = data_proj->reduce(*set_project);
+              //data_project = dynamic_cast<const RooAbsData*>(it->getObject(1));
+            }
+            
+            // create the new projection argument
+            *it = ProjWData(*set_project,
+                            *data_project,
+                            it->getInt(0));
+
+            // sdebug << "Projection data: " << endmsg;
+            // data_project->Print();
+          }
+        }
+        RooArgSet set_project_local(sim_cat);
+        if (!project_arg_found) plot.AddPlotArg(ProjWData(set_project_local,sub_data));
+
+        // RooCmdArg arg_slice = Slice(*category);
+        // plot.AddPlotArg(arg_slice);
+        
+        doocore::lutils::setStyle("LHCb");
+        config_plot_.OnDemandOpenPlotStack();
+        TCanvas c1("c1","c1",900,900);
+        std::string label_text1 = std::string("Next plots: dimension ") + dimension_.GetName() + ", category " + cat_type->GetName();
+        std::string label_text2 = std::string(" (") + plot_name + ")";
+        TPaveText label(0.1, 0.25, 0.9, 0.75);
+        label.SetTextSize(0.03);
+        label.AddText(label_text1.c_str());
+        label.AddText(label_text2.c_str());
+        label.SetLineWidth(0.0);
+        label.SetTextAlign(13);
+        label.SetFillColor(0);
+        label.Draw();
+        c1.Print(std::string(config_plot_.plot_directory()+"/pdf/AllPlots"+config_plot_.plot_appendix()+".pdf").c_str());
+        
+        // Only possible way to avoid plotting problems: Do not use NumCPU for complete fit of whole PDF on all data.
+        //plot.set_ignore_num_cpu(true);
+
+//        TStopwatch sw_plot; sw_plot.Start();
+        plot.PlotHandler(sc_y, suffix);
+//        sdebug << "This plot took " << sw_plot << endmsg;
+        
+        if (set_project != NULL) delete set_project;
+        if (data_reduced != NULL && data_reduced != &sub_data) {
+          delete data_reduced;
+        }
+
+        if (binned_projection) {
+          delete data_project;
+        }
+        
+        //++num_slices;
+
+      }
+
+      if (sub_data2 != NULL) {
+        delete sub_data2;
+      }
+    }
+  }
+  //}
+
   if (num_slices > 1) {
     TCanvas c1("c1","c1",900,900);
     std::string label_text1 = std::string("Next plots: dimension ") + dimension_.GetName() + ", summed all categories ";
