7ea2948c5d2f5d964fa0f65d922cb0286a8b2d23
Alex Birnkraut <alex.birnkraut@tu-dortmund.de>
2018-02-02 16:38:37 +0100

    small corrections in bash script

--- .create_doxygen_and_deploy.sh
+++ .create_doxygen_and_deploy.sh
@@ -11,18 +11,22 @@ if [ -d "html" ] && [ -f "html/index.html" ];
 then
 	# add remote ssh-key to key-storage
 	# first add remote host to known hosts
+	echo "Doing ssh-keyscan"
 	ssh-keyscan -t rsa $DEPLOY_HOST 2> /dev/null | sort -u - ~/.ssh/known_hosts -o ~/.ssh/known_hosts
 
+	echo "Decrypting ssh key"
 	# decrypt private shh key
 	openssl aes-256-cbc -K $encrypted_2adf16dc08ac_key -iv $encrypted_2adf16dc08ac_iv -in id_rsa_doofit.enc -out id_rsa_doofit -d
 
+	echo "Starting ssh agent"
 	# start ssh-agent and add the key
 	eval "$(ssh-agent -s)"
 	chmod 600 id_rsa_doofit
 	ssh-add id_rsa_doofit
 
+	echo "Uploading.."
 	# upload site
-	rsync -rq --delete --exclude=".*" ~/out/ $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
+	rsync -rq --delete --exclude=".*" html/ $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
 else
     echo '' >&2
     echo 'Warning: No documentation (html) files have been found!' >&2
