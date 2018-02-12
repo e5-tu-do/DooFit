d05f8426d491d6923645e4f8def5e268ba2eecc3
Alex Birnkraut <alex.birnkraut@tu-dortmund.de>
2018-02-12 10:48:26 +0100

    encrypted ssh key again

--- .create_doxygen_and_deploy.sh
+++ .create_doxygen_and_deploy.sh
@@ -11,14 +11,11 @@ if [ -d "html" ] && [ -f "html/index.html" ];
 then
 	# add remote ssh-key to key-storage
 	# first add remote host to known hosts
-	echo "Doing ssh-keyscan"
 	ssh-keyscan -t rsa $DEPLOY_HOST 2> /dev/null | sort -u - ~/.ssh/known_hosts -o ~/.ssh/known_hosts
 
 	echo "Decrypting ssh key"
 	# decrypt private shh key
 	openssl aes-256-cbc -K $encrypted_b67e795066bc_key -iv $encrypted_b67e795066bc_iv -in id_rsa_doofit.enc -out id_rsa_doofit -d
-
-	echo "Starting ssh agent"
 	# start ssh-agent and add the key
 	eval "$(ssh-agent -s)"
 	chmod 600 id_rsa_doofit
